48c3a34ad13e5a4e0bd1d648c37364b36d27e52d
suenodk <s.dekort@student.fontys.nl>
2020-06-15 17:35:52 +0200

    Added part 1 of the instagram UI

---
+++ instagram_ui/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Landing from './component/landing/Landing';
+
+function App() {
+  return (
+    <div>
+      <Landing/>
+    </div>
+  );
+}
+
+export default App;
