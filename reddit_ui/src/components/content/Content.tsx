f4e92c7acab54d40cb470573b9eab70459b98028
suenodk <s.dekort@student.fontys.nl>
2020-07-01 20:45:25 +0200

    Create Reddit UI Part 3

--- reddit_ui/src/components/content/Content.tsx
+++ reddit_ui/src/components/content/Content.tsx
@@ -2,11 +2,17 @@ import React from "react";
 
 import "./Content.css";
 import TrendingToday from "./trending-today/TrendingToday";
+import MainBar from "./main-bar/MainBar";
+import SideBar from "./side-bar/SideBar";
 
 export default function Content() {
   return (
     <div className="content">
       <TrendingToday />
+      <div className="bars-wrapper">
+        <MainBar/>
+        <SideBar/>
+      </div>
     </div>
   );
 }
---
+++ reddit_ui/src/components/content/side-bar/SideBar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './SideBar.css';
+
+export default function SideBar() {
+    return <div className="side-bar">Side Bar!</div>
+}
