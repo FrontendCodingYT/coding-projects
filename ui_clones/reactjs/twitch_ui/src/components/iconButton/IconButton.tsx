c8b0c5565147f5e73841cb30a8190c956f571186
Suenodk <suenodekort@gmail.com>
2021-01-18 15:35:38 +0100

    Create the Twitch UI with ReactJS - Part 8: Enums!

--- ui_clones/reactjs/twitch_ui/src/components/iconButton/IconButton.tsx
+++ ui_clones/reactjs/twitch_ui/src/components/iconButton/IconButton.tsx
@@ -6,16 +6,34 @@ import "./IconButton.css";
 
 interface Props {
   Icon: OverridableComponent<SvgIconTypeMap<{}, "svg">>
+  tooltipDirection?: TooltipDirection
   tooltip?: string
   number?: Number
 }
 
-export default function IconButton({Icon, tooltip, number}: Props) {
+export enum TooltipDirection {
+  TOP,
+  RIGHT,
+  BOTTOM,
+  LEFT
+}
+
+export default function IconButton({Icon, tooltipDirection, tooltip, number}: Props) {
+  function getTooltip(direction: TooltipDirection | undefined) {
+    if(direction === undefined) {
+      return tooltip && <span className="tooltip tooltip-bottom">{tooltip}</span>
+    } else if(direction === TooltipDirection.BOTTOM) {
+      return tooltip && <span className="tooltip tooltip-bottom">{tooltip}</span>
+    } else if(direction === TooltipDirection.RIGHT) {
+      return tooltip && <span className="tooltip tooltip-right">{tooltip}</span>
+    }
+  }
+  
   return (
     <button className="icon-button">
       {number && <span className="number">{number}</span>}
       <Icon/>
-      {tooltip && <span className="tooltip">{tooltip}</span>}
+      {tooltip && getTooltip(tooltipDirection)}
     </button>
   );
 }
--- ui_clones/reactjs/twitch_ui/src/components/sidebar/Sidebar.tsx
+++ ui_clones/reactjs/twitch_ui/src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import IconButton, { TooltipDirection } from '../iconButton/IconButton';
 
 import './Sidebar.css';
 
+import ArrowBack from "@material-ui/icons/ArrowBack"
+
 export default function Sidebar() {
-    return <div id="sidebar">Hello I am the sidebar!</div>
+    return <div id="sidebar">
+        <div className="top-section"><span>Recommended Channels</span><IconButton tooltip="Collapse" tooltipDirection={TooltipDirection.RIGHT} Icon={ArrowBack}/></div>
+        <div className="users-section"></div>
+        <div className="twitch-message-section"></div>
+    </div>
 }
