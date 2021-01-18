f66dbf0b7d6daaed1ec6abaa76dff7d2d6b0fe34
Suenodk <suenodekort@gmail.com>
2021-01-18 22:11:30 +0100

    Create the Twitch UI with ReactJS - Part 10: The sidebar

--- ui_clones/reactjs/twitch_ui/src/components/user/User.tsx
+++ ui_clones/reactjs/twitch_ui/src/components/user/User.tsx
@@ -1,17 +1,18 @@
 import React from "react";
+import { User as UserModel } from "../../models/user";
 
 import "./User.css";
 
 interface Props {
-    user: any;
+    user: UserModel;
 }
 
 export default function User({user}: Props) {
   return (
     <div className="user">
-      <img src={user.image} className="profile-picture" /><span className="username">{user.username}</span>
+      <img alt="Bunny" src={user.image} className="profile-picture" /><span className="username">{user.username}</span>
       <span className="description">{user.description}</span>
-      <span className="watchers"><span className="dot">🔴</span>{user.watchers}</span>
+      <span className="watchers"><span className="dot" role="img" aria-label="Amount of people watching">🔴</span>{user.watchers}</span>
     </div>
   );
 }
