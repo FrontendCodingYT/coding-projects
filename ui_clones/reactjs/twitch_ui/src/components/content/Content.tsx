4e13b457063d41387af89b4c930896afee194daa
Suenodk <suenodekort@gmail.com>
2021-01-26 16:07:25 +0100

    Create the Twitch UI with ReactJS - Part 13: JSX to HTML

--- ui_clones/reactjs/twitch_ui/src/components/content/Content.tsx
+++ ui_clones/reactjs/twitch_ui/src/components/content/Content.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import "./Content.css";
+import "../video/Video.css";
 
 import videos from "../../data/videos.json";
+import { Video as VideoModel } from "../../models/video";
 import Video from "../video/Video";
 
 import ChevronLeft from "@material-ui/icons/ChevronLeft";
@@ -10,13 +12,100 @@ import ChevronRight from "@material-ui/icons/ChevronRight";
 import IconButton from "../iconButton/IconButton";
 
 export default function Content() {
-  let index = 0;
+  const slideShowRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const element_left_2 = createVideoElement(videos[0]);
+    element_left_2.classList.add("video-left-2")
+    const element_left_1 = createVideoElement(videos[1]);
+    element_left_1.classList.add("video-left-1")
+    const element_center = createVideoElement(videos[2]);
+    element_center.classList.add("video-center")
+    const element_right_1 = createVideoElement(videos[3]);
+    element_right_1.classList.add("video-right-1")
+    const element_right_2 = createVideoElement(videos[4]);
+    element_right_2.classList.add("video-right-2")
+
+    if (slideShowRef.current) {
+      slideShowRef.current.children[0].insertAdjacentElement("afterend", element_right_2);
+      slideShowRef.current.children[0].insertAdjacentElement("afterend", element_right_1);
+      slideShowRef.current.children[0].insertAdjacentElement("afterend", element_center);
+      slideShowRef.current.children[0].insertAdjacentElement("afterend", element_left_1);
+      slideShowRef.current.children[0].insertAdjacentElement("afterend", element_left_2);
+    }
+  }, []);
+
+  function createVideoElement(video: VideoModel) {
+    const element = document.createElement("div");
+    element.className = "video";
+
+    const videoElement = document.createElement("video");
+    videoElement.src = video.video;
+    videoElement.controls = true;
+
+    const sideMenuElement = document.createElement("div");
+    sideMenuElement.className = "side-menu";
+
+    const imageElement = document.createElement("img");
+    imageElement.className = "profile-picture";
+    imageElement.src = video.image;
+
+    const usernameElement = document.createElement("span");
+    usernameElement.className = "username";
+    usernameElement.innerHTML = video.username;
+
+    const categoryElement = document.createElement("span");
+    categoryElement.className = "category";
+    categoryElement.innerHTML = video.category;
+
+    const watchersElement = document.createElement("span");
+    watchersElement.className = "watchers";
+    watchersElement.innerHTML = "58 viewers";
+
+    const tagsElement = document.createElement("div");
+    tagsElement.className = "tags";
+    video.tags.forEach((t) => {
+      const buttonElement = document.createElement("button");
+
+      buttonElement.className = "button tertiary-button";
+      buttonElement.innerHTML = t;
+
+      tagsElement.appendChild(buttonElement);
+    });
+
+    const descriptionElement = document.createElement("span");
+    descriptionElement.className = "description";
+    descriptionElement.innerHTML = video.description;
+
+    element.appendChild(videoElement);
+    element.appendChild(sideMenuElement);
+
+    sideMenuElement.appendChild(imageElement);
+    sideMenuElement.appendChild(usernameElement);
+    sideMenuElement.appendChild(categoryElement);
+    sideMenuElement.appendChild(watchersElement);
+    sideMenuElement.appendChild(tagsElement);
+    sideMenuElement.appendChild(descriptionElement);
+
+    return element;
+    //       <div className="tags">{video.tags.map(t => {return <Button tertiary label={t}/>})}</div>
+    //       <span className="description">{video.description}</span>
+    //   </div>
+    // </div>
+  }
+
+  function previous() {
+    console.log("previous");
+  }
+
+  function next() {
+    console.log("next");
+  }
   return (
     <div id="content">
-      <div className="video-slideshow">
-        <IconButton Icon={ChevronLeft} />
-        <Video video={videos[index]} />
-        <IconButton Icon={ChevronRight} />
+      <div ref={slideShowRef} className="video-slideshow">
+        <IconButton onClick={previous} Icon={ChevronLeft} />
+        <IconButton onClick={next} Icon={ChevronRight} />
       </div>
     </div>
   );
--- ui_clones/reactjs/twitch_ui/src/components/iconButton/IconButton.tsx
+++ ui_clones/reactjs/twitch_ui/src/components/iconButton/IconButton.tsx
@@ -5,34 +5,51 @@ import React from "react";
 import "./IconButton.css";
 
 interface Props {
-  Icon: OverridableComponent<SvgIconTypeMap<{}, "svg">>
-  tooltipDirection?: TooltipDirection
-  tooltip?: string
-  number?: Number
+  Icon: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
+  tooltipDirection?: TooltipDirection;
+  tooltip?: string;
+  number?: Number;
+  onClick?: Function;
 }
 
 export enum TooltipDirection {
   TOP,
   RIGHT,
   BOTTOM,
-  LEFT
+  LEFT,
 }
 
-export default function IconButton({Icon, tooltipDirection, tooltip, number}: Props) {
+export default function IconButton({
+  Icon,
+  tooltipDirection,
+  tooltip,
+  number,
+  onClick,
+}: Props) {
   function getTooltip(direction: TooltipDirection | undefined) {
-    if(direction === undefined) {
-      return tooltip && <span className="tooltip tooltip-bottom">{tooltip}</span>
-    } else if(direction === TooltipDirection.BOTTOM) {
-      return tooltip && <span className="tooltip tooltip-bottom">{tooltip}</span>
-    } else if(direction === TooltipDirection.RIGHT) {
-      return tooltip && <span className="tooltip tooltip-right">{tooltip}</span>
+    if (direction === undefined) {
+      return (
+        tooltip && <span className="tooltip tooltip-bottom">{tooltip}</span>
+      );
+    } else if (direction === TooltipDirection.BOTTOM) {
+      return (
+        tooltip && <span className="tooltip tooltip-bottom">{tooltip}</span>
+      );
+    } else if (direction === TooltipDirection.RIGHT) {
+      return (
+        tooltip && <span className="tooltip tooltip-right">{tooltip}</span>
+      );
     }
   }
-  
+
+  function executeOnClick(e: React.MouseEvent) {
+    if (onClick) onClick(e);
+  }
+
   return (
-    <button className="icon-button">
+    <button onClick={(e) => executeOnClick(e)} className="icon-button">
       {number && <span className="number">{number}</span>}
-      <Icon/>
+      <Icon />
       {tooltip && getTooltip(tooltipDirection)}
     </button>
   );
