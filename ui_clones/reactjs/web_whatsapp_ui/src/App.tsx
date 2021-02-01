d8a9e9123ffc1fa182179e0d617322d2ca74b5b0
Suenodk <suenodekort@gmail.com>
2021-02-01 18:28:19 +0100

    Create the Web Whatsapp UI with ReactJS - Part 1: Layout

---
+++ ui_clones/reactjs/web_whatsapp_ui/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Chatbox from "./components/chatbox/Chatbox"
+
+function App() {
+  return (
+    <div className="app">
+      <Chatbox/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ui_clones/reactjs/web_whatsapp_ui/src/components/chatbox/Chatbox.tsx
@@ -0,0 +1,14 @@
+import "./Chatbox.css";
+
+export default function Chatbox() {
+  return (
+    <div className="chatbox">
+      <div className="navigation"></div>
+      <div className="search"></div>
+      <div className="list"></div>
+      <div className="details"></div>
+      <div className="chat"></div>
+      <div className="message"></div>
+    </div>
+  );
+}
