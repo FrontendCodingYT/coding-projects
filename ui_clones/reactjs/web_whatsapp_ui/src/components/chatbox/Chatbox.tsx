7d9ef53a11189d7a9372b39983ac943c472472c8
Suenodk <suenodekort@gmail.com>
2021-02-02 16:02:51 +0100

    Create the Web Whatsapp UI with ReactJS - Part 5: Contact details

--- ui_clones/reactjs/web_whatsapp_ui/src/components/chatbox/Chatbox.tsx
+++ ui_clones/reactjs/web_whatsapp_ui/src/components/chatbox/Chatbox.tsx
@@ -3,37 +3,40 @@ import "./Chatbox.css";
 import contacts from "../../data/contacts.json";
 
 import Contact from "../contact/Contact";
+import Details from "../details/Details";
 
 import MessageIcon from "@material-ui/icons/Message";
 import MoreVertIcon from "@material-ui/icons/MoreVert"
 import SearchIcon from "@material-ui/icons/Search";
 
+
 export default function Chatbox() {
   const [navigationOpen, setNavigationOpen] = useState(false);
+  const [selectedContact, setSelectedContact] = useState(contacts[0]);
 
   return (
     <div className={`chatbox ${(navigationOpen) ? 'chatbox-navigation-open' : 'chatbox-navigation-closed'}`}>
-      <div className="navigation">
+      <div className="navigation-section">
         <img className="profile-picture" src="assets/images/Frontend_Coding.png"/>
         <div className="actions">
           <MessageIcon/>
           <MoreVertIcon/>
         </div>
       </div>
-      <div className="search">
+      <div className="search-section">
         <label htmlFor="searchbar">
           <SearchIcon/>
         </label>
         <input id="searchbar" placeholder="Search contacts"/>
       </div>
-      <div className="list">
+      <div className="list-section">
         {contacts.map((c) => {
           return <Contact contact={c}/>
         })}
       </div>
-      <div className="details"></div>
-      <div className="chat"></div>
-      <div className="message"></div>
+      <Details contact={selectedContact}/>
+      <div className="chat-section"></div>
+      <div className="message-section"></div>
     </div>
   );
 }
---
+++ ui_clones/reactjs/web_whatsapp_ui/src/components/details/Details.tsx
@@ -0,0 +1,28 @@
+import "./Details.css";
+
+import { Contact as ContactModel } from "../../models/Contact";
+
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+interface Props {
+  contact: ContactModel;
+}
+
+export default function Details({contact}: Props) {
+  return (
+    <div className="details-section">
+      <div className="user">
+        <img className="profile-picture" src={contact.profile_image} />
+        <div className="details">
+          <span className="name">{contact.name}</span>
+          <span className="last-seen">Time</span>
+        </div>
+      </div>
+      <div className="actions">
+        <AttachFileIcon />
+        <MoreVertIcon />
+      </div>
+    </div>
+  );
+}
