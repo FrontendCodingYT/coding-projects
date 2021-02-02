9b07117ae4b996757b5fe574025b8f51860a1c88
Suenodk <suenodekort@gmail.com>
2021-02-02 12:21:49 +0100

    Create the Web Whatsapp UI with ReactJS - Part 2: Navigation section

--- ui_clones/reactjs/web_whatsapp_ui/src/components/chatbox/Chatbox.tsx
+++ ui_clones/reactjs/web_whatsapp_ui/src/components/chatbox/Chatbox.tsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import "./Chatbox.css";
 
+import MessageIcon from "@material-ui/icons/Message";
+import MoreVertIcon from "@material-ui/icons/MoreVert"
+
 export default function Chatbox() {
+  const [navigationOpen, setNavigationOpen] = useState(false);
+
   return (
-    <div className="chatbox">
-      <div className="navigation"></div>
+    <div className={`chatbox ${(navigationOpen) ? 'chatbox-navigation-open' : 'chatbox-navigation-closed'}`}>
+      <div className="navigation">
+        <img className="profile-picture" src="assets/images/Frontend_Coding.png"/>
+        <div className="actions">
+          <MessageIcon/>
+          <MoreVertIcon/>
+        </div>
+      </div>
       <div className="search"></div>
       <div className="list"></div>
       <div className="details"></div>
