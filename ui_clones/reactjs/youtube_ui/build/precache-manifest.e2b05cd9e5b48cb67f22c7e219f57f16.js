aaf8534a1e95da0fdf6e4fdc39222e5b9963f599
Suenodk <suenodekort@gmail.com>
2021-03-11 20:45:18 +0100

    created build folder and homepage url

---
+++ ui_clones/reactjs/youtube_ui/build/precache-manifest.e2b05cd9e5b48cb67f22c7e219f57f16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a409779a27994837a9ef004678e38848",
+    "url": "/coding-projects/ui_clones/reactjs/youtube_ui/build/index.html"
+  },
+  {
+    "revision": "98f2b9e04a120e3d650c",
+    "url": "/coding-projects/ui_clones/reactjs/youtube_ui/build/static/css/main.03ebd5c4.chunk.css"
+  },
+  {
+    "revision": "8710ef1fde4160220e10",
+    "url": "/coding-projects/ui_clones/reactjs/youtube_ui/build/static/js/2.370e9d40.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/coding-projects/ui_clones/reactjs/youtube_ui/build/static/js/2.370e9d40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98f2b9e04a120e3d650c",
+    "url": "/coding-projects/ui_clones/reactjs/youtube_ui/build/static/js/main.2f7f7c49.chunk.js"
+  },
+  {
+    "revision": "6dad209f67ad894113f1",
+    "url": "/coding-projects/ui_clones/reactjs/youtube_ui/build/static/js/runtime-main.16fb21cd.js"
+  }
+]);
