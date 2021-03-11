f70b512565a37ade61fdfcc4a7a5425f3773ea55
Suenodk <suenodekort@gmail.com>
2021-03-11 20:56:17 +0100

    created build folder and added homepage url. Also compressed the video and the image!

---
+++ ui_clones/reactjs/twitch_ui/build/precache-manifest.e6273e1fd13d96a9ce0b211c3f7af797.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce98463d027a18122d0cf03fa4d2ddb",
+    "url": "/coding-projects/ui_clones/reactjs/twitch_ui/build/index.html"
+  },
+  {
+    "revision": "9912bd52f65b76f95f3d",
+    "url": "/coding-projects/ui_clones/reactjs/twitch_ui/build/static/css/main.e1415dc9.chunk.css"
+  },
+  {
+    "revision": "96fe3351f3fd8933821f",
+    "url": "/coding-projects/ui_clones/reactjs/twitch_ui/build/static/js/2.2135fa23.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/coding-projects/ui_clones/reactjs/twitch_ui/build/static/js/2.2135fa23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9912bd52f65b76f95f3d",
+    "url": "/coding-projects/ui_clones/reactjs/twitch_ui/build/static/js/main.09ea5077.chunk.js"
+  },
+  {
+    "revision": "b6ef44fc243f350bad4a",
+    "url": "/coding-projects/ui_clones/reactjs/twitch_ui/build/static/js/runtime-main.6a19e55b.js"
+  },
+  {
+    "revision": "1f6704ced87ac1e13633cb5da83f18ea",
+    "url": "/coding-projects/ui_clones/reactjs/twitch_ui/build/static/media/twitch_logo.1f6704ce.png"
+  }
+]);
