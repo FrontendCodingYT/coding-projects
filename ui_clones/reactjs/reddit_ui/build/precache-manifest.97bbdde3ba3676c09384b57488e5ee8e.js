0bfa6aba8a347ddf12015559e13fe725ba57bdb9
Suenodk <suenodekort@gmail.com>
2021-03-11 20:26:49 +0100

    added build folder and homepage url

---
+++ ui_clones/reactjs/reddit_ui/build/precache-manifest.97bbdde3ba3676c09384b57488e5ee8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ee2c854b9f6e77c8433addac1429a4",
+    "url": "/coding-projects/ui_clones/reactjs/reddit_ui/build/index.html"
+  },
+  {
+    "revision": "0acecda39d980ca9fbe9",
+    "url": "/coding-projects/ui_clones/reactjs/reddit_ui/build/static/css/main.88eb3ab2.chunk.css"
+  },
+  {
+    "revision": "a93cbffe39f4de595fd3",
+    "url": "/coding-projects/ui_clones/reactjs/reddit_ui/build/static/js/2.2bbc98ea.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/coding-projects/ui_clones/reactjs/reddit_ui/build/static/js/2.2bbc98ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0acecda39d980ca9fbe9",
+    "url": "/coding-projects/ui_clones/reactjs/reddit_ui/build/static/js/main.c75feae4.chunk.js"
+  },
+  {
+    "revision": "6836cad0da753199f476",
+    "url": "/coding-projects/ui_clones/reactjs/reddit_ui/build/static/js/runtime-main.fcfa6e1e.js"
+  }
+]);
