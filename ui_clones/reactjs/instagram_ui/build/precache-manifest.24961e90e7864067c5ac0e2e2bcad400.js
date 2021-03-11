ab073cef2198f06c4fe9956c09a58743fa2e38f7
Suenodk <suenodekort@gmail.com>
2021-03-11 18:36:37 +0100

    compressed videos and images, also added the build folder

---
+++ ui_clones/reactjs/instagram_ui/build/precache-manifest.24961e90e7864067c5ac0e2e2bcad400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5ecbbdbfeaf949b378643873b0116d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9679973f64586bc42248",
+    "url": "/static/css/main.fff9463d.chunk.css"
+  },
+  {
+    "revision": "48cecab5ee34b73c11db",
+    "url": "/static/js/2.fe2c3a82.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fe2c3a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9679973f64586bc42248",
+    "url": "/static/js/main.15fcef6b.chunk.js"
+  },
+  {
+    "revision": "75f31621340dd475dfe0",
+    "url": "/static/js/runtime-main.e76c7b67.js"
+  }
+]);
