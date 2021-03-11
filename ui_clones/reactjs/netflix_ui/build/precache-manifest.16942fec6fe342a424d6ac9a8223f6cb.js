402e4c40ec3e7de9cc55e834e1cafb9fa4357304
Suenodk <suenodekort@gmail.com>
2021-03-11 20:08:03 +0100

    compressed images and created build folder

---
+++ ui_clones/reactjs/netflix_ui/build/precache-manifest.16942fec6fe342a424d6ac9a8223f6cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4f5ef5a035cd0f640c90e33b78582b",
+    "url": "/coding-projects/ui_clones/reactjs/netflix_ui/build/index.html"
+  },
+  {
+    "revision": "c4157e9f6b23e26d5f72",
+    "url": "/coding-projects/ui_clones/reactjs/netflix_ui/build/static/css/main.3b8e739b.chunk.css"
+  },
+  {
+    "revision": "2303ffce38093d3ef412",
+    "url": "/coding-projects/ui_clones/reactjs/netflix_ui/build/static/js/2.a0c51f97.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/coding-projects/ui_clones/reactjs/netflix_ui/build/static/js/2.a0c51f97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4157e9f6b23e26d5f72",
+    "url": "/coding-projects/ui_clones/reactjs/netflix_ui/build/static/js/main.2be31b68.chunk.js"
+  },
+  {
+    "revision": "b014b7de5d97416718f2",
+    "url": "/coding-projects/ui_clones/reactjs/netflix_ui/build/static/js/runtime-main.3a61317f.js"
+  }
+]);
