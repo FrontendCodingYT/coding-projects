38091e8bf089dfb5bf794cc0f05091079bcb0f45
Suenodk <suenodekort@gmail.com>
2021-03-22 15:13:36 +0100

    Create the Discord UI with ReactJS - Part 3: Responsive grid

--- ui_clones/reactjs/discord_ui/src/pages/Home/Home.tsx
+++ ui_clones/reactjs/discord_ui/src/pages/Home/Home.tsx
@@ -1,11 +1,27 @@
 import "./Home.css";
 
+//Breaking point is 1200px
+//Larger than 1200px display right
+//Else don't display right
+
+//Tsx
+//Remove the element from the dom tree
+
+//Css
+//Display: none, so the element is not visible anymore but still in the dom tree
+
 export default function Home() {
     return <div className="home-page">
-        <div>Navigation</div>
-        <div>Unclaimed account</div>
-        <div>Top row</div>
-        <div>Main row</div>
-        <div>Bottom row</div>
+        <nav>Navigation</nav>
+        <div className="unclaimed">Unclaimed account</div>
+        <div className="top-left">Top left</div>
+        <div className="top-main">Top main</div>
+        <div className="top-right">Top right</div>
+        <div className="main-left">Main left row</div>
+        <div className="main">Main row</div>
+        <div className="main-right">Main right row</div>
+        <div className="bottom-left">Bottom left row</div>
+        <div className="bottom-main">Bottom row</div>
+        <div className="bottom-right">Bottom right row</div>
     </div>
 }
