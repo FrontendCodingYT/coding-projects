c2eb65949478b198b50e6cadf977846454064ffc
Suenodk <suenodekort@gmail.com>
2021-03-22 19:36:35 +0100

    Create the Discord UI with ReactJS - Part 4: Discord button

--- ui_clones/reactjs/discord_ui/src/pages/Home/Home.tsx
+++ ui_clones/reactjs/discord_ui/src/pages/Home/Home.tsx
@@ -1,27 +1,42 @@
 import "./Home.css";
-
-//Breaking point is 1200px
-//Larger than 1200px display right
-//Else don't display right
-
 //Tsx
 //Remove the element from the dom tree
 
 //Css
 //Display: none, so the element is not visible anymore but still in the dom tree
 
+//Discord button
+//Divider
+//Other buttons -> Component
+
+import discordLogo from "../../images/Discord logo.png";
+
 export default function Home() {
-    return <div className="home-page">
-        <nav>Navigation</nav>
-        <div className="unclaimed">Unclaimed account</div>
-        <div className="top-left">Top left</div>
-        <div className="top-main">Top main</div>
-        <div className="top-right">Top right</div>
-        <div className="main-left">Main left row</div>
-        <div className="main">Main row</div>
-        <div className="main-right">Main right row</div>
-        <div className="bottom-left">Bottom left row</div>
-        <div className="bottom-main">Bottom row</div>
-        <div className="bottom-right">Bottom right row</div>
+  return (
+    <div className="home-page">
+      <nav>
+        <div className="discord-button">
+            <div className="indicator"></div>
+          <button>
+            <img src={discordLogo} />
+          </button>
+        </div>
+        <div>divider</div>
+        <button>Server</button>
+        <button>Public</button>
+        <div>divider</div>
+        <button>Download</button>
+      </nav>
+      <div className="unclaimed">Unclaimed account</div>
+      <div className="top-left">Top left</div>
+      <div className="top-main">Top main</div>
+      <div className="top-right">Top right</div>
+      <div className="main-left">Main left row</div>
+      <div className="main">Main row</div>
+      <div className="main-right">Main right row</div>
+      <div className="bottom-left">Bottom left row</div>
+      <div className="bottom-main">Bottom row</div>
+      <div className="bottom-right">Bottom right row</div>
     </div>
+  );
 }
