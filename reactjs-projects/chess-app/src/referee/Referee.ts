0d40b2a32eaa6bc444018368e7db1f563029749c
Suenodk <suenodekort@gmail.com>
2021-07-22 14:57:23 +0200

    Create a Chess game with Reactjs part 14: Bishop attack

--- reactjs-projects/chess-app/src/referee/Referee.ts
+++ reactjs-projects/chess-app/src/referee/Referee.ts
@@ -170,53 +170,66 @@ export default class Referee {
         //Up right movement
         if(desiredPosition.x > initialPosition.x && desiredPosition.y > initialPosition.y) {
           let passedPosition: Position = {x: initialPosition.x + i, y: initialPosition.y + i};
-          if(this.tileIsOccupied(passedPosition, boardState)) {
-            console.log("Illegal move");
-            break;
+          //Check if the tile is the destination tile
+          if(passedPosition.x === desiredPosition.x && passedPosition.y === desiredPosition.y) {
+            //Dealing with destination tile
+            if(this.tileIsEmptyOrOccupiedByOpponent(passedPosition, boardState, team)) {
+              return true;
+            }
+          } else {
+            //Dealing with passing tile
+            if(this.tileIsOccupied(passedPosition, boardState)) {
+              break;
+            }
           }
-        }
-        
-        if(desiredPosition.x - initialPosition.x === i && desiredPosition.y - initialPosition.y === i) {
-          return true;
         }
         
         //Bottom right movement
         if(desiredPosition.x > initialPosition.x && desiredPosition.y < initialPosition.y) {
           let passedPosition: Position = {x: initialPosition.x + i, y: initialPosition.y - i};
-          if(this.tileIsOccupied(passedPosition, boardState)) {
-            console.log("Illegal move");
-            break;
+          //Check if the tile is the destination tile
+          if(passedPosition.x === desiredPosition.x && passedPosition.y === desiredPosition.y) {
+            //Dealing with destination tile
+            if(this.tileIsEmptyOrOccupiedByOpponent(passedPosition, boardState, team)) {
+              return true;
+            }
+          } else {
+            if(this.tileIsOccupied(passedPosition, boardState)) {
+              break;
+            }
           }
-        }
-
-        if(desiredPosition.x - initialPosition.x === i && desiredPosition.y - initialPosition.y === -i) {
-          return true;
         }
 
         //Bottom left movement
         if(desiredPosition.x < initialPosition.x && desiredPosition.y < initialPosition.y) {
           let passedPosition: Position = {x: initialPosition.x - i, y: initialPosition.y - i};
-          if(this.tileIsOccupied(passedPosition, boardState)) {
-            console.log("Illegal move");
-            break;
+          //Check if the tile is the destination tile
+          if(passedPosition.x === desiredPosition.x && passedPosition.y === desiredPosition.y) {
+            //Dealing with destination tile
+            if(this.tileIsEmptyOrOccupiedByOpponent(passedPosition, boardState, team)) {
+              return true;
+            }
+          } else {
+            if(this.tileIsOccupied(passedPosition, boardState)) {
+              break;
+            }
           }
-        }
-
-        if(desiredPosition.x - initialPosition.x === -i && desiredPosition.y - initialPosition.y === -i) {
-          return true;
         }
 
         //Top left movement
         if(desiredPosition.x < initialPosition.x && desiredPosition.y > initialPosition.y) {
           let passedPosition: Position = {x: initialPosition.x - i, y: initialPosition.y+i};
-          if(this.tileIsOccupied(passedPosition, boardState)) {
-            console.log("Illegal move");
-            break;
+          //Check if the tile is the destination tile
+          if(passedPosition.x === desiredPosition.x && passedPosition.y === desiredPosition.y) {
+            //Dealing with destination tile
+            if(this.tileIsEmptyOrOccupiedByOpponent(passedPosition, boardState, team)) {
+              return true;
+            }
+          } else {
+            if(this.tileIsOccupied(passedPosition, boardState)) {
+              break;
+            }
           }
-        }
-
-        if(desiredPosition.x - initialPosition.x === -i && desiredPosition.y - initialPosition.y === i) {
-          return true;
         }
       }
     }
