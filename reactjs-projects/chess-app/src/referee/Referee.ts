4bfcf7696cb27eaa9106a370e102c0cbdade1b86
Suenodk <suenodekort@gmail.com>
2021-07-22 15:34:22 +0200

    Create a Chess game with Reactjs part 15: Rook logic

--- reactjs-projects/chess-app/src/referee/Referee.ts
+++ reactjs-projects/chess-app/src/referee/Referee.ts
@@ -232,6 +232,34 @@ export default class Referee {
           }
         }
       }
+    } else if(type === PieceType.ROOK) {
+      if(initialPosition.x === desiredPosition.x) {
+        console.log("Moving vertically!");
+
+        for(let i = 1; i < 8; i++) {
+          let multiplier = (desiredPosition.y < initialPosition.y) ? -1 : 1;
+
+          let passedPosition: Position = {x: initialPosition.x, y: initialPosition.y + (i * multiplier)}; 
+          if(passedPosition.x === desiredPosition.x && passedPosition.y === desiredPosition.y) {
+            console.log("Arrived!");
+            break;
+          }
+        }
+      }
+
+      if(initialPosition.y === desiredPosition.y) {
+        console.log("Moving horizontally!");
+
+        for(let i = 1; i < 8; i++) {
+          let multiplier = (desiredPosition.x < initialPosition.x) ? -1 : 1;
+
+          let passedPosition: Position = {x: initialPosition.x + (i * multiplier), y: initialPosition.y};
+          if(passedPosition.x === desiredPosition.x && passedPosition.y === desiredPosition.y) {
+            console.log("Arrived!");
+            break;
+          }
+        }
+      }
     }
 
     return false;
